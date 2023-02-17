70dbc8eb3738fa62a55b9ad87780caa38cf91c16
tpxDeppy <despoina.tsounaka@tpximpact.com>
2023-02-17 16:14:21 +0000

    add queries

---
+++ Queries.sql
@@ -0,0 +1,79 @@
+-- QUERIES
+
+-- - selecting a list of orders
+--     - how would you order this by order date desc
+
+SELECT TOP(100) 
+    Order_ID AS [Order ID],
+    Order_date AS [Date ordered],
+    Total_products AS [Total products],
+    Total_price AS [Total cost],
+    Order_status AS [Order status],
+    Order_modified AS [Last modified],
+    Customer_ID AS [Customer ID]
+FROM [Order]
+ORDER BY Order_date DESC;
+
+
+
+-- - selecting a list of one users orders
+
+SELECT Order_ID AS [Order ID],
+    Order_date AS [Date ordered],
+    Total_products AS [Total products],
+    Total_price AS [Total cost],
+    Order_status AS [Order status],
+    Order_modified AS [Last modified],
+    Customer_ID AS [Customer ID]
+FROM [Order] 
+WHERE Customer_ID LIKE 'b2e99c1d-2e1f-4dea-ac1a-cf2bac749851';
+
+
+--OR--
+SELECT Order_ID AS [Order ID],
+    Last_name AS [Last name],
+    Order_date AS [Date ordered],
+    Total_products AS [Total products],
+    Total_price AS [Total cost],
+    Order_status AS [Order status],
+    Order_modified AS [Last modified]
+FROM [Order] O
+INNER JOIN Customer C ON O.Customer_ID = C.Customer_ID
+WHERE Last_name LIKE 'Millhouse';
+
+
+
+-- - selecting a list of one users orders and the items in the order
+
+SELECT 
+    First_name + ' ' + Last_name AS [Customer's full name],
+    Product_name AS [Products in the order]
+FROM [Order] O
+INNER JOIN Product_Order PO ON O.Order_ID = PO.Order_ID
+INNER JOIN Customer C ON O.Customer_ID = C.Customer_ID
+INNER JOIN Product P ON PO.Product_ID = P.Product_ID
+WHERE Last_name LIKE 'Apple';
+
+
+
+-- - selecting a list of products of type food
+
+SELECT TOP(100)
+    Product_name AS [Product name],
+    Category_name AS [Type]
+FROM Product P
+INNER JOIN Product_Category PC ON P.Product_ID = PC.Product_ID
+INNER JOIN Category C ON PC.Category_ID = C.Category_ID
+WHERE Category_name LIKE 'Food'; 
+
+
+
+-- - selecting a list of all vegan food products
+
+SELECT 
+    Product_name AS [Product name],
+    Type_of AS [Dietary Requirement]
+FROM Dietary_Requirement DR
+INNER JOIN Dietary_Product DP ON DR.D_Requirement_ID = DP.D_Requirement_ID 
+INNER JOIN Product P ON DP.Product_ID = P.Product_ID
+WHERE Type_of LIKE 'Vegan';
